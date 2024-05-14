58ef0370027d8ac58983ef17387b0b3da38726a8
Vytautas Kontrimas <vytautas.kontrimas@outlook.com>
2024-05-14 23:51:12 +0100

    render clear screen

--- glue/src/main.cpp
+++ glue/src/main.cpp
@@ -1,4 +1,5 @@
 #include <SDL.h>
+#include <glad/gl.h>
 #include <glog/logging.h>
 
 int main(int argc, char** argv) {
@@ -8,10 +9,34 @@ int main(int argc, char** argv) {
   const auto sdl_init_error = SDL_Init(SDL_INIT_VIDEO);
   CHECK(sdl_init_error == 0) << "Failed to initialize SDL: " << SDL_GetError();
 
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+  // max opengl version on mac os
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
+
+  SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, SDL_TRUE);
+
+  SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+  SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
+
+  SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
+  SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
+  SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
+  SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8);
   SDL_Window* window =
       SDL_CreateWindow("Window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
                        1280, 720, SDL_WINDOW_ALLOW_HIGHDPI | SDL_WINDOW_OPENGL);
-  CHECK(window) << "Couldn't create window: " << SDL_GetError();
+  CHECK(window) << SDL_GetError();
+  SDL_ShowWindow(window);
+
+  SDL_GLContext gl_context = SDL_GL_CreateContext(window);
+  CHECK(gl_context) << SDL_GetError();
+
+  const auto gl_version =
+      gladLoadGL(reinterpret_cast<GLADloadfunc>(SDL_GL_GetProcAddress));
+  CHECK(gl_version != 0) << "Failed to load GL";
+  LOG(INFO) << "GL version " << GLAD_VERSION_MAJOR(gl_version) << "."
+            << GLAD_VERSION_MINOR(gl_version);
 
   bool is_running = true;
   while (is_running) {
@@ -25,9 +50,14 @@ int main(int argc, char** argv) {
       }
     }
 
+    glClearColor(0.0f, 0.4f, 0.6f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+
     SDL_GL_SwapWindow(window);
   }
 
+  SDL_GL_DeleteContext(gl_context);
+  SDL_DestroyWindow(window);
   SDL_Quit();
   return 0;
 }
