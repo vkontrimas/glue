55bd6a012babbdc116ecb83981ad35ac04597ba7
Vytautas Kontrimas <vytautas.kontrimas@outlook.com>
2024-05-14 23:18:16 +0100

    init glog

--- glue/src/main.cpp
+++ glue/src/main.cpp
@@ -1,2 +1,10 @@
+#include <glog/logging.h>
 
-int main(int argc, char **argv) {}
+int main(int argc, char **argv) {
+  google::InitGoogleLogging(argv[0]);
+  FLAGS_alsologtostderr = 1;
+
+  LOG(INFO) << "Hello world";
+
+  return 0;
+}
