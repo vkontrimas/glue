c79810170960fb2627afdaf3817929867de7fdfa
Vytautas Kontrimas <vytautas.kontrimas@outlook.com>
2024-05-25 20:05:15 +0100

    buffer inputs and world states

--- libgame/include/glue/simulator/predictor_reconciler_simulator.hpp
+++ libgame/include/glue/simulator/predictor_reconciler_simulator.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <glue/collections/circular_buffer.hpp>
 #include <glue/debug/idata_logger.hpp>
 #include <glue/debug/timer.hpp>
 #include <glue/director/igame_director.hpp>
@@ -22,8 +23,8 @@ class PredictorReconcilerSimulator final : public ISimulator {
         timestep_{timestep},
         buffer_frames_{std::max(2ul, static_cast<std::size_t>(glm::ceil(
                                          buffer_duration / timestep)))},
-        past_frame_{initial_frame},
-        future_frame_{initial_frame} {}
+        frame_buffer_{buffer_frames(), {initial_frame}},
+        input_buffer_{buffer_frames(), {Input{}}} {}
 
   virtual void update(f64 delta_time, Input& input) override {
     auto logger = debug::NoOpDataLogger<f64>{};
@@ -34,32 +35,50 @@ class PredictorReconcilerSimulator final : public ISimulator {
   void update_timed(f64 delta_time, Input& input, TDataLogger& logger) {
     timestep_.update(delta_time, [&](f64 timestep) {
       debug::Timer timer;
-      std::memcpy(&past_frame_, &future_frame_, sizeof(WorldFrame));
 
-      future_frame_.active_cubes.clear();
+      if (input_buffer_.full()) {
+        input_buffer_.pop_front();
+      }
+      input_buffer_.push_back(input);
 
-      director_->pre_physics(timestep, input, future_frame_);
-      physics_->step(timestep, future_frame_);
-      director_->post_physics(timestep, input, future_frame_);
+      if (frame_buffer_.full()) {
+        frame_buffer_.pop_front();
+      }
+      frame_buffer_.emplace_back();
+      // copy prev frame into future one as we're building on top of it
+      std::memcpy(&frame_buffer_[frame_buffer_.size() - 1],
+                  &frame_buffer_[frame_buffer_.size() - 2], sizeof(WorldFrame));
+
+      auto& future_frame = frame_buffer_[frame_buffer_.size() - 1];
+      future_frame.active_cubes.clear();
+
+      director_->pre_physics(timestep, input, future_frame);
+      physics_->step(timestep, future_frame);
+      director_->post_physics(timestep, input, future_frame);
       logger.log(timer.elapsed_ms<f64>());
+
+      ++current_frame_;
     });
   }
 
   virtual void current_world_frame(WorldFrame& frame) override {
-    WorldFrame::interpolate(past_frame_, future_frame_, timestep_.alpha(),
-                            frame);
+    WorldFrame::interpolate(frame_buffer_[frame_buffer_.size() - 2],
+                            frame_buffer_[frame_buffer_.size() - 1],
+                            timestep_.alpha(), frame);
   }
 
   const std::size_t buffer_frames() const noexcept { return buffer_frames_; }
+  const std::size_t current_frame() const noexcept { return current_frame_; }
 
  private:
   std::shared_ptr<director::IGameDirector> director_;
   std::shared_ptr<physics::IPhysicsEngine> physics_;
   FixedTimestep timestep_;
   std::size_t buffer_frames_;
+  std::size_t current_frame_ = 0;
 
   // TODO(vkon): circular buffer
-  WorldFrame past_frame_;
-  WorldFrame future_frame_;
+  CircularBuffer<WorldFrame> frame_buffer_;
+  CircularBuffer<Input> input_buffer_;
 };
 }  // namespace glue::simulator
