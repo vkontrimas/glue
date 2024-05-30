cee197fe250ddf0d6a79fa98ec491c42eed280d9
Vytautas Kontrimas <vytautas.kontrimas@outlook.com>
2024-05-30 07:34:57 +0100

    make all bitpackers constexpr

--- libbitpack/include/glue/bitpack/bitpack.hpp
+++ libbitpack/include/glue/bitpack/bitpack.hpp
@@ -5,17 +5,29 @@
 #include <type_traits>
 
 namespace glue::bitpack {
-inline void pack(Packer& packer, u8& value) { packer.write_bits(value, 8); }
-inline void pack(Unpacker& packer, u8& value) { value = packer.read_bits(8); }
+inline constexpr void pack(Packer& packer, u8& value) {
+  packer.write_bits(value, 8);
+}
+inline constexpr void pack(Unpacker& packer, u8& value) {
+  value = packer.read_bits(8);
+}
 
-inline void pack(Packer& packer, u16& value) { packer.write_bits(value, 16); }
-inline void pack(Unpacker& packer, u16& value) { value = packer.read_bits(16); }
+inline constexpr void pack(Packer& packer, u16& value) {
+  packer.write_bits(value, 16);
+}
+inline constexpr void pack(Unpacker& packer, u16& value) {
+  value = packer.read_bits(16);
+}
 
-inline void pack(Packer& packer, u32& value) { packer.write_bits(value, 32); }
-inline void pack(Unpacker& packer, u32& value) { value = packer.read_bits(32); }
+inline constexpr void pack(Packer& packer, u32& value) {
+  packer.write_bits(value, 32);
+}
+inline constexpr void pack(Unpacker& packer, u32& value) {
+  value = packer.read_bits(32);
+}
 
 template <CPacker TPacker>
-inline void pack(TPacker& packer, u64& val) {
+inline constexpr void pack(TPacker& packer, u64& val) {
   u32 high_bits{static_cast<u32>((val >> 32) & 0xffffffff)};
   u32 low_bits{static_cast<u32>(val & 0xffffffff)};
   pack(packer, high_bits);
@@ -24,7 +36,7 @@ inline void pack(TPacker& packer, u64& val) {
 }
 
 template <CPacker TPacker, std::signed_integral T>
-inline void pack(TPacker& packer, T& val) {
+inline constexpr void pack(TPacker& packer, T& val) {
   using UnsignedT = std::make_unsigned_t<T>;
   UnsignedT uval = static_cast<UnsignedT>(val);
   pack(packer, uval);
@@ -32,7 +44,7 @@ inline void pack(TPacker& packer, T& val) {
 }
 
 template <CPacker TPacker>
-inline void pack(TPacker& packer, f32& val) {
+inline constexpr void pack(TPacker& packer, f32& val) {
   // UB but how it's done in game industry afaik.
   union {
     f32 float_val;
@@ -44,7 +56,7 @@ inline void pack(TPacker& packer, f32& val) {
 }
 
 template <CPacker TPacker>
-inline void pack(TPacker& packer, f64& val) {
+inline constexpr void pack(TPacker& packer, f64& val) {
   // UB but how it's done in game industry afaik.
   union {
     f64 float_val;
@@ -55,6 +67,10 @@ inline void pack(TPacker& packer, f64& val) {
   val = float_val;
 }
 
-inline void pack(Packer& packer, bool val) { packer.write_bits(val, 1); }
-inline void pack(Unpacker& packer, bool& val) { val = packer.read_bits(1); }
+inline void constexpr pack(Packer& packer, bool val) {
+  packer.write_bits(val, 1);
+}
+inline void constexpr pack(Unpacker& packer, bool& val) {
+  val = packer.read_bits(1);
+}
 }  // namespace glue::bitpack
