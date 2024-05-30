61d0d03038ab1538b1d3fa17fcf1ee59e536754d
Vytautas Kontrimas <vytautas.kontrimas@outlook.com>
2024-05-30 07:12:25 +0100

    bitpack signed integers

--- libbitpack/include/glue/bitpack/bitpack.hpp
+++ libbitpack/include/glue/bitpack/bitpack.hpp
@@ -2,6 +2,7 @@
 
 #include <concepts>
 #include <glue/bitpack/packer.hpp>
+#include <type_traits>
 
 namespace glue::bitpack {
 inline void pack(Packer& packer, u8& value) { packer.write_bits(value, 8); }
@@ -13,13 +14,21 @@ inline void pack(Unpacker& packer, u16& value) { value = packer.read_bits(16); }
 inline void pack(Packer& packer, u32& value) { packer.write_bits(value, 32); }
 inline void pack(Unpacker& packer, u32& value) { value = packer.read_bits(32); }
 
-template <CPacker T>
-inline void pack(T& packer, u64& val) {
+template <CPacker TPacker>
+inline void pack(TPacker& packer, u64& val) {
   u32 high_bits{static_cast<u32>((val >> 32) & 0xffffffff)};
   u32 low_bits{static_cast<u32>(val & 0xffffffff)};
   pack(packer, high_bits);
   pack(packer, low_bits);
   val = (u64{high_bits} << 32) | low_bits;
+}
+
+template <CPacker TPacker, std::signed_integral T>
+inline void pack(TPacker& packer, T& val) {
+  using UnsignedT = std::make_unsigned_t<T>;
+  UnsignedT uval = static_cast<UnsignedT>(val);
+  pack(packer, uval);
+  val = static_cast<T>(uval);
 }
 
 inline void pack(Packer& packer, bool val) { packer.write_bits(val, 1); }
--- libbitpack/tests/bitpack/test_bitpack.cpp
+++ libbitpack/tests/bitpack/test_bitpack.cpp
@@ -48,10 +48,38 @@ constexpr TestData<u64> test_data() {
            0xc, 0x5e810db8, 0xffffffff, 0xffffffff}};
 }
 
+template <>
+constexpr TestData<i8> test_data() {
+  return {{-128, -10, -2, 1, 2, 10, 127}, {0x80f6'fe01, 0x020a'7f00}};
+}
+
+template <>
+constexpr TestData<i16> test_data() {
+  return {{-32768, -16322, 1521, 32767}, {0x8000'c03e, 0x05f1'7fff}};
+}
+
+template <>
+constexpr TestData<i32> test_data() {
+  return {{-2147483648, -121421421, 5123512, 2147483647},
+          {static_cast<u32>(-2147483648), static_cast<u32>(-121421421), 5123512,
+           2147483647}};
+}
+
+template <>
+constexpr TestData<i64> test_data() {
+  return {
+      {static_cast<i64>(
+           0x8000000000000000) /* can't get a min i64 in on my compiler :( */,
+       -1235123512353232, 511234, 32512351235213, 9223372036854775807},
+      {0x8000'0000, 0x0000'0000, 0xfffb'9ca9, 0x65ca'de30, 0x0000'0000,
+       0x0007'cd02, 0x0000'1d91, 0xdf25'6c8d, 0x7fff'ffff, 0xffff'ffff}};
+}
+
 template <class T>
 class BitpackTests : public ::testing::Test {};
 
-using BitpackTypes = ::testing::Types<u8, u16, u32, u64, bool>;
+using BitpackTypes =
+    ::testing::Types<u8, u16, u32, u64, i8, i16, i32, i64, bool>;
 TYPED_TEST_SUITE(BitpackTests, BitpackTypes);
 
 TYPED_TEST(BitpackTests, Packs) {
