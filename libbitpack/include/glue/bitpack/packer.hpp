20209c9a2c4e7871a9f4c53d8785119f335f18de
Vytautas Kontrimas <vytautas.kontrimas@outlook.com>
2024-05-30 04:45:47 +0100

    write a packer class for bitpacking

---
+++ libbitpack/include/glue/bitpack/packer.hpp
@@ -0,0 +1,102 @@
+#pragma once
+
+#include <concepts>
+#include <glue/assert.hpp>
+#include <glue/types.hpp>
+#include <span>
+
+namespace glue::bitpack::detail {
+class BasePacker {
+ public:
+  using size_t = std::size_t;
+  using value_t = u32;
+
+  static constexpr size_t kValueSizeBits = sizeof(value_t) * 8;
+
+ public:
+  constexpr BasePacker() = default;
+  virtual ~BasePacker() = default;
+
+  explicit constexpr BasePacker(std::span<value_t> data) noexcept
+      : data_{data} {}
+
+  constexpr size_t capacity() const noexcept { return data_.size(); }
+  constexpr size_t capacity_bits() const noexcept {
+    return capacity() * kValueSizeBits;
+  }
+
+  constexpr size_t current() const noexcept {
+    return current_bit() / kValueSizeBits;
+  }
+  constexpr size_t current_bit() const noexcept { return bit_position_; }
+
+  constexpr size_t next() const noexcept { return current() + 1; }
+
+ protected:
+  std::span<value_t> data_{};
+  size_t bit_position_{0};
+};
+}  // namespace glue::bitpack::detail
+
+namespace glue::bitpack {
+struct Packer final : public detail::BasePacker {
+  constexpr Packer() = default;
+  explicit constexpr Packer(std::span<value_t> data) noexcept
+      : detail::BasePacker{data} {}
+
+  /*
+   * Write count bits and advance position.
+   */
+  constexpr void write_bits(value_t value, size_t count) {
+    glue_assert(count <= kValueSizeBits);
+    glue_assert(current_bit() + count < capacity_bits());
+
+    value &= static_cast<value_t>((1 << count) - 1);
+
+    constexpr auto kAlignMask = kValueSizeBits - 1;
+    const size_t space = kValueSizeBits - (current_bit() & kAlignMask);
+    if (space >= count) {
+      const auto shift = space - count;
+      data_[current()] |= value << shift;
+      bit_position_ += count;
+    } else {
+      const value_t high_value = value >> (count - space);
+      write_bits(high_value, space);
+      write_bits(value, count - space);
+    }
+  }
+};
+
+struct Unpacker final : public detail::BasePacker {
+  constexpr Unpacker() = default;
+  explicit constexpr Unpacker(std::span<value_t> data) noexcept
+      : detail::BasePacker{data} {}
+
+  /*
+   * Read count bits and advance position.
+   */
+  constexpr value_t read_bits(size_t count) {
+    glue_assert(count <= kValueSizeBits);
+    glue_assert(current_bit() + count < capacity_bits());
+
+    const value_t value_mask = static_cast<value_t>((1 << count) - 1);
+
+    constexpr auto kAlignMask = kValueSizeBits - 1;
+    const size_t space = kValueSizeBits - (current_bit() & kAlignMask);
+    if (space >= count) {
+      const auto shift = space - count;
+      const value_t value = (data_[current()] >> shift) & value_mask;
+      bit_position_ += count;
+      return value;
+    } else {
+      const value_t high_mask = (1 << space) - 1;
+      const value_t high_value = read_bits(space);
+      const value_t low_value = read_bits(count - space);
+      return (high_value << (count - space)) | low_value;
+    }
+  }
+};
+
+template <class T>
+concept CPacker = std::derived_from<T, detail::BasePacker>;
+}  // namespace glue::bitpack
---
+++ libbitpack/tests/bitpack/test_packer.cpp
@@ -0,0 +1,107 @@
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include <array>
+#include <glue/bitpack/packer.hpp>
+
+using namespace glue;
+using namespace glue::bitpack;
+using namespace ::testing;
+
+template <std::derived_from<glue::bitpack::detail::BasePacker> T>
+class BasePackerTests : public ::testing::Test {};
+
+using PackerTypes = ::testing::Types<Packer, Unpacker>;
+TYPED_TEST_SUITE(BasePackerTests, PackerTypes);
+
+TYPED_TEST(BasePackerTests, WhenDefaultConstructed_ZeroCapacityAndSizes) {
+  TypeParam packer;
+  EXPECT_EQ(packer.capacity(), 0);
+  EXPECT_EQ(packer.capacity_bits(), 0);
+  EXPECT_EQ(packer.current(), 0);
+  EXPECT_EQ(packer.current_bit(), 0);
+}
+
+TYPED_TEST(BasePackerTests, WhenBackedByArray_CapacityDerivedFromArray) {
+  std::array<u32, 200> data;
+
+  TypeParam packer{data};
+  EXPECT_EQ(packer.capacity(), 200);
+  EXPECT_EQ(packer.capacity_bits(), 6400);
+  EXPECT_EQ(packer.current(), 0);
+  EXPECT_EQ(packer.current_bit(), 0);
+}
+
+TEST(PackerTests, WriteBitsCorrectly) {
+  std::array<u32, 2> data{{0, 0}};
+  Packer packer{data};
+
+  packer.write_bits(0b0101'0110'1111'0101, 16);
+  EXPECT_EQ(packer.current(), 0);
+  EXPECT_EQ(packer.current_bit(), 16);
+
+  packer.write_bits(0b1111'0101'11, 10);
+  EXPECT_EQ(packer.current(), 0);
+  EXPECT_EQ(packer.current_bit(), 26);
+
+  packer.write_bits(0x0, 32);
+  EXPECT_EQ(packer.current(), 1);
+  EXPECT_EQ(packer.current_bit(), 58);
+
+  packer.write_bits(0b11, 2);
+  EXPECT_EQ(packer.current(), 1);
+  EXPECT_EQ(packer.current_bit(), 60);
+
+  EXPECT_THAT(data, ElementsAre(0b0101'0110'1111'0101'1111'0101'1100'0000,
+                                0b0000'0000'0000'0000'0000'0000'0011'0000));
+}
+
+TEST(UnpackerTests, ReadBitsCorrectly) {
+  std::array<u32, 2> data{{0b0101'0110'1111'0101'1111'0101'1100'0000,
+                           0b0000'0000'0000'0000'0000'0000'0011'0000}};
+  Unpacker packer{data};
+
+  EXPECT_EQ(packer.read_bits(16), 0b0101'0110'1111'0101);
+  EXPECT_EQ(packer.current(), 0);
+  EXPECT_EQ(packer.current_bit(), 16);
+
+  EXPECT_EQ(packer.read_bits(10), 0b1111'0101'11);
+  EXPECT_EQ(packer.current(), 0);
+  EXPECT_EQ(packer.current_bit(), 26);
+
+  EXPECT_EQ(packer.read_bits(32), 0);
+  EXPECT_EQ(packer.current(), 1);
+  EXPECT_EQ(packer.current_bit(), 58);
+
+  EXPECT_EQ(packer.read_bits(2), 0b11);
+  EXPECT_EQ(packer.current(), 1);
+  EXPECT_EQ(packer.current_bit(), 60);
+}
+
+TEST(PackerDeathTests, WhenWritingMoreThan32Bits_Die) {
+  std::array<u32, 24> data;
+  Packer packer{data};
+  EXPECT_DEATH(packer.write_bits(0b000, 33), "Assertion.*");
+}
+
+TEST(PackerDeathTests, WhenWritingOutOfBounds_Die) {
+  std::array<u32, 2> data;
+  Packer packer{data};
+  packer.write_bits(0x0, 32);
+  packer.write_bits(0x0, 16);
+  EXPECT_DEATH(packer.write_bits(0x0, 32), "Assertion.*");
+}
+
+TEST(UnpackerDeathTests, WhenReadingMoreThan32Bits_Die) {
+  std::array<u32, 24> data;
+  Unpacker packer{data};
+  EXPECT_DEATH(packer.read_bits(33), "Assertion.*");
+}
+
+TEST(UnpackerDeathTests, WhenReadingOutOfBounds_Die) {
+  std::array<u32, 2> data;
+  Unpacker packer{data};
+  packer.read_bits(32);
+  packer.read_bits(16);
+  EXPECT_DEATH(packer.read_bits(32), "Assertion.*");
+}
